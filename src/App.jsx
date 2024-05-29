2ba1111a4ccf8de9bcefced6f713772e354c7cf5
Артем Соболев <sobolev_am@MacBook-Pro-Artem.local>
2024-05-29 19:54:27 +0500

    added search

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import data from "./assets/data.json";
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="wrapper">
-                <Header/>
-                <div className="cards-container">
-                    {data.map((d, i) => <Card 
-                        key={i}
-                        img={d.picture}
-                        text={d.name}
-                        price={d.price}
-                    />)}
-                </div>
+const App = () => {
+    const [goods, setGoods] = useState(data);
+    
+    return (
+        <div className="wrapper">
+            <Header products={data} update={setGoods}/>
+            <div className="cards-container">
+                {goods.map((d, i) => <Card 
+                    key={i}
+                    img={d.picture}
+                    text={d.name}
+                    price={d.price}
+                />)}
             </div>
-        )
-    }
+        </div>
+    )
+    
 }
 
 export default App;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 import Logo from "../logo"
+import {ReactComponent as FavIcon} from "./img/ic_favorites.svg";
+import {ReactComponent as CartIcon} from "./img/ic_cart.svg";
+import {ReactComponent as ProfileIcon} from "./img/ic_profile.svg";
 
-export default () => {
+export default ({products, update}) => {
+    const [text, changeText] = useState("");
+    const [cnt, setCnt] = useState(0);
+    const handler = e => {
+        changeText(e.target.value);
+        const result = products.filter((el => el.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1));
+        setCnt(result.length);
+        if (!text) {
+            update(products);
+        } else {
+            update(result);
+        }
+    }
     return (
-        <header>
-            <Logo/>
-            <input type="search"/>
-            <nav>
-                <a href="">Favorits</a>
-                <a href="">Cart</a>
-                <a href="">Profile</a>
-            </nav>
-        </header>
+        <>
+            <header>
+                <Logo/>
+                <input type="search" value={text} onChange={handler}/>
+                <nav>
+                    <a href=""><FavIcon/></a>
+                    <a href=""><CartIcon/></a>
+                    <a href=""><ProfileIcon/></a>
+                </nav>
+            </header>
+            <div>
+                {text ? `По запросу ${text} найдено ${cnt} позиций` : "Пойск..."}
+            </div>
+        </>
     )
 };
